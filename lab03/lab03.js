3c7b8c3a1abb6020d84fd58272e86b497c3c13bf
mgetz137 <32659060+mgetz137@users.noreply.github.com>
2018-09-19 17:51:51 -0400

    Update lab03.js

--- lab03/lab03.js
+++ lab03/lab03.js
@@ -1,1 +1,16 @@
 
+/*
+/Matthew Getz
+/lab03
+/cs336
+/9-19-18
+*/
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('site'))
+app.use('/static', express.static('site'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
