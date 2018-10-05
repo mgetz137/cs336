c15ddb949e2946639826ab7e96c3608fd79844fc
mgetz137 <32659060+mgetz137@users.noreply.github.com>
2018-10-05 10:35:21 -0400

    Create homework1.js

---
+++ homework1/homework1.js
@@ -0,0 +1,15 @@
+/*
+/Matthew Getz
+/homework1
+/cs336
+/10-6-18
+*/
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('site'))
+app.use('/static', express.static('site'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
