bf1b00762c96adf9ad287389212b7b55c306b5cc
Matthew Getz <mdg7@gold23.cs.calvin.edu>
2018-11-09 16:24:40 -0500

    10.2 done

--- lab10/server.js
+++ lab10/server.js
@@ -10,22 +10,47 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+
+
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-var COMMENTS_FILE = path.join(__dirname, 'comments.json');
-
+var MongoClient = require('mongodb').MongoClient;
 app.set('port', (process.env.PORT || 3000));
+var db;
+
+MongoClient.connect('mongodb://cs336:' + process.env.MONGO_PASSWORD + '@ds255463.mlab.com:55463/cs336', function (err, client) {
+    if (err) {
+        throw err;
+    }
+
+    db = client.db('cs336');
+
+    db.collection('comments').find().toArray(function (err, result) {
+        if (err) throw err
+		
+		db = result;
+
+
+    })
+
+    app.listen(app.get('port'), function () {
+        console.log('Server started: http://localhost:' + app.get('port') + '/');
+
+
+    })
+});
+
 
 app.use('/', express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Additional middleware which will set headers that we need on each request.
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     // Set permissive CORS header - this allows this server to be used only as
     // an API server in conjunction with something like webpack-dev-server.
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -35,44 +60,36 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.get('/api/comments', function(req, res) {
-    fs.readFile(COMMENTS_FILE, function(err, data) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        res.json(JSON.parse(data));
+
+
+
+app.get('/api/comments', function (req, res) {
+    // Get the documents collection
+    var collection = db.collection('comments');
+    // Find some documents
+    collection.find({}).toArray(function (err, docs) {
+        //console.log("Found the following records");
+        //console.log(docs)
+        res.json(docs);
     });
 });
 
-app.post('/api/comments', function(req, res) {
-    fs.readFile(COMMENTS_FILE, function(err, data) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
 
-        var comments = JSON.parse(data);
-        // NOTE: In a real implementation, we would likely rely on a database or
-        // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
-        // treat Date.now() as unique-enough for our purposes.
-        var newComment = {
+
+app.post('/api/comments', function (req, res) {
+
+    // Get the documents collection
+    var collection = db.collection('comments');
+    // Insert some documents
+    collection.insertOne({
             id: Date.now(),
             author: req.body.author,
             text: req.body.text,
-        };
-        comments.push(newComment);
-        fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
-            if (err) {
-                console.error(err);
-                process.exit(1);
-            }
-            res.json(comments);
+        },
+        function (err, result) {
+            console.log("Inserted a comment");
+            res.json((result));
         });
-    });
-});
 
 
-app.listen(app.get('port'), function() {
-    console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
